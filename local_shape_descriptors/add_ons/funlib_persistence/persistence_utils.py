544dd87939a3ecf800b9b59bd175514ee9353c17
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2024-02-23 17:19:22 +0000

    #add: prepare_ds_daisy

--- local_shape_descriptors/add_ons/funlib_persistence/persistence_utils.py
+++ local_shape_descriptors/add_ons/funlib_persistence/persistence_utils.py
@@ -1,7 +1,7 @@
 from funlib.persistence import prepare_ds
 from funlib.geometry import Roi, Coordinate
 import logging
-from data_utils.preprocess_volumes.pad_input import pad_input 
+from data_utils.preprocess_volumes.pad_input import pad_input
 
 # get the same logger that is defined in parent script
 logger = logging.getLogger(__name__)
@@ -16,6 +16,8 @@ def prepare_predict_datasets(cfg, dtype, ds_key, ds_size, total_roi, voxel_size,
     total_roi = Roi(total_roi.get_offset(), total_roi.get_shape())
     if not total_roi.shape.is_multiple_of(voxel_size):
         print(f"total roi {total_roi} is not a multiple of voxel_size: {voxel_size}")
+        logger.debug(f"total roi {total_roi} is not a multiple of voxel_size: {voxel_size}")
+
     prepare_ds(
         filename=cfg.DATA.OUTFILE,
         ds_name=ds_key,
@@ -25,4 +27,30 @@ def prepare_predict_datasets(cfg, dtype, ds_key, ds_size, total_roi, voxel_size,
         write_size=ds_size,
         num_channels=num_channels,
         delete=delete_ds  # always delete existing datasets
+    )
+
+
+def prepare_predict_datasets_daisy(cfg, dtype, ds_key, source_roi, voxel_size, write_roi, num_channels=None,
+                                   delete_ds=False):
+    """With `funlib.persistence` create the .zarr dataset placeholders for output"""
+
+    logger.warning("Warning: The datasets if they exist in the output  will always be re-created. "
+                   "Hence pass delete_ds=False if you wish to preserve them and only add something new.")
+
+    total_roi = Roi(source_roi.get_offset(), source_roi.get_shape())
+    print(total_roi)
+    if not total_roi.shape.is_multiple_of(voxel_size):
+        print(f"total roi {total_roi} is not a multiple of voxel_size: {voxel_size}")
+        logger.debug(f"total roi {total_roi} is not a multiple of voxel_size: {voxel_size}")
+
+    prepare_ds(
+        filename=cfg.DATA.OUTFILE,
+        ds_name=ds_key,
+        total_roi=total_roi,  # this is output roi == source roi
+        voxel_size=voxel_size,
+        dtype=dtype,
+        write_roi=write_roi,
+        num_channels=num_channels,
+        delete=delete_ds,  # don't delete because multiple workers will be writing to the same dataset
+        compressor='default'
     )
