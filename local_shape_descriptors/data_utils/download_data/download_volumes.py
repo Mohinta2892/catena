fbc834bc237a91ee65c02d8e98984ebcbc102d0c
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2024-03-29 14:30:04 +0000

    update downloading

--- local_shape_descriptors/data_utils/download_data/download_volumes.py
+++ local_shape_descriptors/data_utils/download_data/download_volumes.py
@@ -1,79 +1,71 @@
-"""Full tutorial to be devised after sign-up to aws is complete!!
-Tutorial to include:
-1. AWS Signup process
-2. Parsing keys for each volume type. Potentially port some code from Arlo's lsd_nm_experiments.
-
 """
+Step 1: Configure your AWS access and secret key.
+On a bash shell run:
+$ aws configure # creds get stored here:  ~/.aws/credentials
+$ vim ~/.bash_profile
+AWS_SECRET_ACCESS_KEY = 'same key you set with aws configure'
+AWS_ACCESS_KEY_ID = 'same key you set with aws configure'
+$ source ~/.bash_profile
+
+Step 2: Edit the volumes in Line 55 below if needed.
+Step3: Run fetch_data.py
+"""
+
 import boto3
-import bson
-import numpy as np
+import json
+import multiprocessing as mp
 import os
-import pandas as pd
-import zarr
-from cloudvolume import CloudVolume
+from botocore import UNSIGNED
+from botocore.config import Config
 
-# set bucket credentials
-access_key = 'put_your_key'
-secret_key = 'put_your_key'
-bucket = 'open-neurodata'
+client = None
 
-session = boto3.session.Session(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key
-)
 
-client = session.client('s3')
+def initialize(access_key, secret_key):
+    global client
 
-# connect to client
-# client = session.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-
-# list data
-print(client.list_objects(Bucket=bucket, Prefix="funke"))
-
-# download directory structure file - this shows exactly how the s3 data is stored
-if not os.path.exists("./structure.md"):
-    client.download_file(
-        Bucket=bucket,
-        Key="funke/structure.md",
-        Filename="structure.md")
-
-# visualize the directory structure to fetch selectively/custom use
-with open('./structure.md', 'r') as f:
-    text = f.read()
-    html = markdown.markdown(text)
-
-with open('structure.html', 'w') as f:
-    f.write(html)
+    client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 
 
 # function to download all files nested in a bucket path
-def download_directory(
-        bucket_name,
-        path,
-        access_key,
-        secret_key):
-    resource = boto3.resource(
-        's3',
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key)
+def download_data(job):
+    bucket_name, path = job
+
+    resource = boto3.resource("s3", config=Config(signature_version=UNSIGNED))
 
     bucket = resource.Bucket(bucket_name)
 
     for obj in bucket.objects.filter(Prefix=path):
-        if not os.path.exists(os.path.dirname(obj.key)):
-            os.makedirs(os.path.dirname(obj.key))
+        os.makedirs(os.path.dirname(obj.key), exist_ok=True)
 
         key = obj.key
 
-        print(f'Downloading {key}')
+        print(f"Downloading {key}")
         bucket.download_file(key, key)
 
 
-if __name__ == '__main__':
-    # download example hemi/zebrafinch/fib25 training data
-    # download_directory(
-    #     bucket,
-    #     'funke/hemi/testing/ground_truth/data.zarr',
-    #     access_key,
-    #     secret_key)
-    pass
+if __name__ == "__main__":
+    bucket = "open-neurodata"
+
+    # load training data
+    with open("datasets.json") as f:
+        config = json.load(f)
+
+    # just test with one volume for each dataset.
+    volumes = {
+        #"fib25": config["fib25"][0:1],
+        #"hemi": config["hemi"][0:1],
+        "zebrafinch": config["zebrafinch"]#[0:1],
+    }
+
+    jobs = [(bucket, f"funke/{d}/training/{x}") for d, v in volumes.items() for x in v]
+    access_key='samiamohinta'
+    secret_key='lsd_data_download'
+    # download each volume with separate process, would want to adapt to work
+    # with more processes if downloading more than 3 volumes..
+    pool = mp.Pool(len(jobs), initialize(access_key, secret_key))
+
+    pool.map(download_data, jobs)
+
+    pool.close()
+    pool.join()
