00457146f87fd2a65a2bb74852b136708266c28a
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2024-04-02 19:35:19 +0100

    convert hdf5 to zarr
    All models need zarr files

---
+++ local_shape_descriptors/data_utils/download_data/hdf_to_zarr.py
@@ -0,0 +1,60 @@
+import h5py
+import numpy as np
+import zarr
+import os
+import sys
+
+from glob import glob
+import argparse
+import numcodecs
+from tqdm import tqdm
+
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().cwd()))
+
+from utils import list_keys
+
+
+def hdf_to_zarr(hdf, out_dir):
+    hf = h5py.File(hdf)
+    datasets = list_keys(hf)
+
+    # create out zarr
+    zf_path = os.path.join(out_dir, os.path.basename(hdf).split('.hdf')[0] + '.zarr')
+    zf = zarr.open(zf_path, mode='a')
+    for ds in tqdm(datasets, desc='iterating over datasets'):
+        if hf[ds].dtype == object:
+            # print(list(hf[ds][...]))
+            zf.create_dataset(name=ds, data=tuple(hf[ds][...]), shape=hf[ds].shape, dtype=object, object_codec=numcodecs.VLenBytes(),
+                              overwrite=True)
+        else:
+            zf[ds] = hf[ds]
+        for k in hf[ds].attrs.keys():
+            zf[ds].attrs[k] = tuple(hf[ds].attrs[k])  # casting prevents - `object cannot be serialized error`
+
+    # make a labels mask for the labels - required by lsds
+    labels_mask_name = 'volumes/labels/labels_mask'
+    labels_mask = np.ones_like(zf["volumes/labels/neuron_ids"][...])
+    background = zf["volumes/labels/neuron_ids"] == 0
+    labels_mask[background] = 0
+
+    zf[labels_mask_name] = labels_mask
+    zf[labels_mask_name].attrs['offset'] = zf["volumes/labels/neuron_ids"].attrs['offset']
+    zf[labels_mask_name].attrs['resolution'] = zf["volumes/labels/neuron_ids"].attrs['resolution']
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Converts hdf to zarr")
+    parser.add_argument('-d', help="/directory/path/to/hdf")
+    parser.add_argument('-od', help="/output/directory/path/to/hdf")
+
+    args = parser.parse_args()
+
+    all_hdfs = sorted(glob(f"{args.d}/*.h*"))
+    for h in all_hdfs:
+        hdf_to_zarr(h, args.od)
+
+
+if __name__ == '__main__':
+    main()
