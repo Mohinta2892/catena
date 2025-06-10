e97513528157ddc0e398da980dd736f69974abce
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2025-06-10 20:37:29 +0100

    add viz with mito for syn verification

---
+++ visualize/visualize_nglancer_synapses_mito_hdf.py
@@ -0,0 +1,388 @@
+"""
+This script can be used to visualise hdf files containing synapse ground truths with neuron segmentation and clefts.
+One needs to (un)comment portions of code corresponding to the datasets present in the hdf.
+Tested on: CREMI unpadded datasets.
+For padded datasets one must pass the correct voxel_offset to the segmentation layers (WIP).
+
+Major Requirements and tested with:
+python==3.10
+neuroglancer==2.37
+
+Original credits: Adapted and updated based on Julia Buhmann's script under synful_experiments.
+Author: Samia Mohinta
+Affiliation: Cardona lab, Cambridge UK
+"""
+
+import argparse
+import itertools
+import os
+
+import daisy
+import h5py
+import zarr
+import neuroglancer
+import numpy as np
+from funlib.persistence import open_ds  # open_ds is broken for hdf file loading;
+
+# neuroglancer.set_server_bind_address('0.0.0.0')
+ngid = itertools.count(start=1)
+
+
+def load_zarr(inputfilename, dataset):
+    f = zarr.open(inputfilename, mode='r')
+
+    offset = (0, 0, 0)
+    if dataset in f:
+        data = f[dataset][:]
+        if 'offset' in f[dataset].attrs.keys():
+            offset = f[dataset].attrs['offset']
+        print(dataset, data.shape, data.dtype)
+    else:
+        data = None
+        print(dataset, 'does not exist')
+
+    # f.close()
+    return data, offset
+
+
+def load_hdf5(inputfilename, dataset):
+    f = h5py.File(inputfilename, 'r')
+    offset = (0, 0, 0)
+    if dataset in f:
+        data = f[dataset][:]
+        if 'offset' in f[dataset].attrs.keys():
+            offset = f[dataset].attrs['offset']
+        print(dataset, data.shape, data.dtype)
+    else:
+        data = None
+        print(dataset, 'does not exist')
+    f.close()
+    return data, offset
+
+
+def ngLayer(data, res, oo=(0, 0, 0), tt='segmentation'):
+    """Adds a local volume layer to the current state of the viewer."""
+    return neuroglancer.LocalVolume(data, dimensions=res, volume_type=tt, voxel_offset=oo)
+
+
+def add(s, a, name, shader=None, data=None):
+    """
+    This function from old version of this script has been retained for now to have compatibility to neuroglancer <=1.5.
+    Will be removed eventually.
+    """
+    if a is None:
+        return
+
+    if shader == 'rgb':
+        shader = """void main() { emitRGB(vec3(toNormalized(getDataValue(0)),
+        toNormalized(getDataValue(1)), toNormalized(getDataValue(2)))); }"""
+
+    kwargs = {}
+
+    if shader is not None:
+        kwargs['shader'] = shader
+    data = a.data if data is None else data
+
+    s.layers.append(name=name, layer=neuroglancer.LocalVolume(data, dimensions=dimensions, volume_type='image',
+                                                              voxel_offset=(0, 0, 0)))
+
+
+def open_ds_wrapper(path, ds_name):
+    """ funlib.persistence `open_ds` is broken for hdf files.
+    Issue raised in Github.
+    """
+    try:
+        return open_ds(path, ds_name)
+    except KeyError:
+        print('dataset %s could not be loaded' % ds_name)
+        return None
+
+
+def add_cremi_synapses(s, filename, res):
+    if filename.lower().endswith((".h5", ".hdf", ".hdf5")):
+        # inelegant loading for now till open_ds gets fixed for hdfs (loads zarrs ok)
+        locs, offsets = load_hdf5(filename, 'annotations/locations')
+    elif filename.lower().endswith(".zarr"):
+        locs, offsets = load_zarr(filename, 'annotations/locations')
+
+    offset = 0
+
+    # convert offsets to voxel coords
+    offsets = [i / j for i, j in zip(offsets, res)]
+
+    # flip locations if data gets loaded as xyz
+    # locs = [np.flip(loc) + offset for loc in locs]
+    # currently data is loaded as zyx
+    locs = [loc + np.array(offsets, dtype=np.float32) for loc in locs]
+    # locs = [loc + offset for loc in locs]
+    # print(locs)
+
+    if filename.lower().endswith((".h5", ".hdf", ".hdf5")):
+        partners, offset = load_hdf5(filename, 'annotations/presynaptic_site/partners')
+        annotation_ids, offset = load_hdf5(filename, 'annotations/ids')
+    elif filename.lower().endswith(".zarr"):
+        partners, offset = load_zarr(filename, 'annotations/presynaptic_site/partners')
+        annotation_ids, offset = load_zarr(filename, 'annotations/ids')
+
+    print(f"partners {partners}")
+    print(f"annotation_ids {annotation_ids}")
+
+    (pre_sites, post_sites, connectors) = ([], [], [])
+    (pre_sites_coords, post_sites_coords, connectors_coords) = ([], [], [])
+    for (pre, post) in partners:
+
+        # Get indices of rows where pre matches the first column of annotation_ids
+        pre_indices = np.where(annotation_ids == pre)[0]
+        # Get indices of rows where post matches the second column of annotation_ids
+        post_indices = np.where(annotation_ids == post)[0]
+
+        # pre_index = int(np.where(pre == annotation_ids)[0][0])
+        # post_index = int(np.where(post == annotation_ids)[0][0])
+
+        # pre_indices = np.where(pre == annotation_ids)[0]
+        # post_indices = np.where(post == annotation_ids)[0]
+        #
+        # if len(pre_indices) == 0 or len(post_indices) == 0:
+        #     print(f"Skipping pair (pre: {pre}, post: {post}) - not found in annotation_ids")
+        #     continue
+
+        pre_index = int(pre_indices[0])
+        post_index = int(post_indices[0])
+
+        # print(pre_index, post_index)#
+        try:
+            pre_site = locs[pre_index]
+            post_site = locs[post_index]
+        except:
+            print(f"Exception: {pre_index, post_index}")
+
+
+        # Calc the distance between every pre-post site
+        dist = np.linalg.norm(
+            np.array(pre_site, dtype=np.float32) - np.array(post_site, dtype=np.float32))
+        if dist > 160:
+            print(pre_site, post_site)
+            print(f"distance: {dist}")
+
+        # if all(pre_site == post_site):
+        #     print(f"common pre post {pre_site, post_site}")
+
+        # get rid of the transpose if the data is already transposed
+        # post_site = [post_site[2], post_site[1], post_site[0]]
+        # pre_site = [pre_site[2], pre_site[1], pre_site[0]]
+        # print(f"pre {pre_site}, post {post_site}")
+
+
+        # pre_sites.append(neuroglancer.EllipsoidAnnotation(center=pre_site,
+        #                                                   radii=(40, 40, 40),
+        #                                                   id=next(ngid)))
+
+
+        post_sites_coords.append(list(post_site))
+        post_sites.append(neuroglancer.PointAnnotation(point=post_site,
+                                                           # radii=(10, 10, 10),
+                                                           id=next(ngid)))
+        # else:
+        #     print("post site exists")
+        # if list(pre_site) not in pre_sites_coords:
+        #     if list(pre_site)[0] == 592:
+        #         print(f"pre_site: {pre_site}")
+                # import pdb
+                # pdb.set_trace()
+        pre_sites_coords.append(list(pre_site))
+        pre_sites.append(neuroglancer.PointAnnotation(point=pre_site,
+                                                          # radii=(10, 10, 10),
+                                                          id=next(ngid)))
+        # else:
+        #     print("pre site exists")
+        # post_sites.append(neuroglancer.PointAnnotation(point=(100, 100, 100),
+        #                                                    # radii=(40, 40, 40),
+        #                                                    id=next(ngid)))
+
+        # if (list(pre_site) in pre_sites_coords) and (list(post_site) in post_sites_coords):
+        connectors.append(
+            neuroglancer.LineAnnotation(point_a=pre_site, point_b=post_site,
+                                        id=next(ngid)))
+
+    #
+    # print(f"Connectors: {connectors}")
+    # print(f"pre_sites: {pre_sites}")
+    # print(f"post sites: {post_sites}")
+
+    s.layers.append(
+        name="connectors",
+        layer=neuroglancer.LocalAnnotationLayer(
+            dimensions=neuroglancer.CoordinateSpace(
+                names=["z", "y", "x"],
+                # names=["x", "y", "z"],
+                units="nm",
+                scales=[1, 1, 1],  # [8, 8, 8],
+            ),
+            annotation_relationships=['connectors'],
+            # linked_segmentation_layer={'connectors': 'segmentation'},
+            # filter_by_segmentation=['connectors'],
+            ignore_null_segment_filter=False,
+            annotation_properties=[
+                neuroglancer.AnnotationPropertySpec(
+                    id='color',
+                    type='rgb',
+                    default='#ffff00',
+                )
+            ],
+            annotations=connectors
+        )
+    )
+
+    s.layers.append(
+        name="pre_sites",
+        layer=neuroglancer.LocalAnnotationLayer(
+            dimensions=neuroglancer.CoordinateSpace(
+                names=["z", "y", "x"],
+                # names=["x", "y", "z"],
+                units="nm",
+                scales=[1, 1, 1],  # [8, 8, 8],
+            ),
+            annotation_relationships=['connectors'],
+            linked_segmentation_layer={'pre_sites': 'segmentation'},
+            filter_by_segmentation=['pre_sites'],
+            ignore_null_segment_filter=False,
+            annotation_properties=[
+                neuroglancer.AnnotationPropertySpec(
+                    id='color',
+                    type='rgb',
+                    default='#FF0000'  # '#ff0000',  # '#ffff00',
+                )
+            ],
+            annotations=pre_sites
+        )
+    )
+
+    s.layers.append(
+        name="post_sites",
+        layer=neuroglancer.LocalAnnotationLayer(
+            dimensions=neuroglancer.CoordinateSpace(
+                names=["z", "y", "x"],
+                # names=["x", "y", "z"],
+                units="nm",
+                scales=[1, 1, 1],  # [8,8,8]
+            ),
+            annotation_relationships=['connectors'],
+            # linked_segmentation_layer={'post_sites': 'segmentation'},
+            # filter_by_segmentation=['post_sites'],
+            ignore_null_segment_filter=False,
+            annotation_properties=[
+                neuroglancer.AnnotationPropertySpec(
+                    id='color',
+                    type='rgb',
+                    default='#ff00ff'  # '#ff00ff',
+                )
+            ],
+            annotations=post_sites
+        )
+    )
+
+
+def resolution_tuple(resolution_str):
+    # Custom function to parse a resolution string into a tuple of integers
+
+    try:
+        parts = resolution_str.split(',')
+        if len(parts) != 3:
+            raise argparse.ArgumentTypeError("Resolution must contain exactly three integers separated by commas.")
+        return tuple(int(part.strip()) for part in parts)
+    except ValueError:
+        raise argparse.ArgumentTypeError("Resolution must contain only integers separated by commas.")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sfile',
+                        default="/media/samia/DATA/mounts/zstore1/catena/data/OCTO_3CUBES_ZYX/data_3d/train/octo_cube3_calyx_5603_6267_y3254_3890_z7464_8163.hdf",
+                        help="Synapse hdf file")
+
+    parser.add_argument('--mfile', default='',
+                        help="Mito segmentations zarr file")
+
+    parser.add_argument('--res', nargs='+', default="8,8,8",
+                        help="Provide the resolution/voxel_size as comma separated numbers of the dataset in ZYX"
+                             " (e.g., `40, 4, 4` ).")
+
+    args = parser.parse_args()
+
+    if isinstance(args.res, list):
+        res = resolution_tuple(args.res[0])
+    elif isinstance(args.res, str):
+        res = resolution_tuple(args.res)
+    else:
+        raise Exception(f"Cannot parse {args.res}")
+
+    # res = resolution_tuple(args.res)
+
+    dimensions = neuroglancer.CoordinateSpace(
+        names=["z", "y", "x"],
+        # names=["x", "y", "z"],
+        units="nm",
+        scales=res,
+    )
+    filename = args.sfile
+    mfilename = args.mfile
+
+    # inelegant loading for now till open_ds gets fixed for hdfs (loads zarrs ok)
+    if filename.lower().endswith((".h5", ".hdf", ".hdf5")):
+        # load raw
+        raw, r_offset = load_hdf5(args.sfile, 'volumes/raw')
+        # raw = raw[:, :, ::-1]
+
+        # load neuron_ids
+        neuron_ids, n_offset = load_hdf5(args.sfile, '/volumes/labels/neuron_ids')
+
+        # load clefts
+        clefts, c_offset = load_hdf5(args.sfile, '/volumes/labels/clefts')
+
+    elif filename.lower().endswith(".zarr"):
+        raw, r_offset = load_zarr(args.sfile, 'volumes/raw')
+        # raw = np.transpose(raw, (2, 1, 0))  # get rid of this if this is already in zyx
+
+        # load neuron_ids
+        neuron_ids, n_offset = load_zarr(args.sfile, '/volumes/labels/neuron_ids')
+        # neuron_ids = np.transpose(neuron_ids, (2, 1, 0))  # get rid of this if this is already in zyx
+
+        # load clefts
+        clefts, c_offset = load_zarr(args.sfile, '/volumes/labels/clefts')
+
+    if mfilename.lower().endswith(".zarr"):
+        print(f"mito filename: {mfilename}")
+        # load neuron_ids
+        mito_ids, n_offset = load_zarr(args.mfile, '/volumes/labels/mito_ids')
+        # neuron_ids = np.transpose(neuron_ids, (2, 1, 0))  # get rid of this if this is already in zyx
+
+    r_offset = [i / j for i, j in zip(r_offset, res)]
+    n_offset = [i / j for i, j in zip(n_offset, res)]
+
+    if clefts is not None:
+        # replace a np.uint64(-3) to 0 as background, helps to visualise better
+        clefts[clefts == np.array(-1).astype(np.uint64)] = 0
+        c_offset = [i / j for i, j in zip(c_offset, res)]
+
+viewer = neuroglancer.Viewer()
+with viewer.txn() as s:
+    # add raw
+    s.layers.append(name='image', layer=ngLayer(raw, dimensions, tt='image', oo=r_offset))
+    # add segmentations
+    if neuron_ids is not None:
+        s.layers.append(name='neuron_ids', layer=ngLayer(neuron_ids, dimensions, tt='segmentation', oo=n_offset))
+    if clefts is not None:
+        s.layers.append(name='clefts', layer=ngLayer(clefts, dimensions, tt='segmentation', oo=c_offset))
+    if mito_ids is not None:
+        s.layers.append(name='mito', layer=ngLayer(mito_ids, dimensions, tt='segmentation', oo=c_offset))
+    # add synapses; they are also in the same hdf file
+    add_cremi_synapses(s, args.sfile, res)
+
+    # adding states as per the old version of this script.
+    # add(s, labels_mask, 'labels_mask')
+
+# change viewer url
+# print(viewer.__str__().replace('c04u01.int.janelia.org', '10.40.4.51'))
+
+# for localhost
+print(viewer)
