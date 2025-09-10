f8b5cd412165d4b71b021f480d330fb814c8b369
Samia Mohinta <44754434+Mohinta2892@users.noreply.github.com>
2025-09-10 10:28:53 +0100

    update cave client to include timestamp extraction

--- proofreading/cave_client_to_load_stacks.py
+++ proofreading/cave_client_to_load_stacks.py
@@ -2,6 +2,8 @@
 This script is a first trial at trying to talk to the FlyWire Octo instance via the CAVEclient.
 Ultimately, we would want to programmatically merged catmaid tracings via the caveclient.
 
+Run (in beast): conda deactivate; conda activate ngl
+
 Author: Samia Mohinta
 Affiliation: Cambridge University, UK
 """
@@ -19,7 +21,7 @@ print(f"My current token is: {auth.token}")
 # # we need to get a token to make the connection
 # print(auth.get_new_token()) # this should show a message
 
-new_token = 'x'  # This is the text you see after you visit the website.
+new_token = 'f6721f14bd4a5ee68df9f725fdb47c92'  # This is the text you see after you visit the website.
 try:
     if new_token != current_token:
         auth.save_token(token=new_token)  # save to disk
@@ -33,17 +35,80 @@ print(client.info.get_datastacks())
 
 # Choose a datastack to initialize with
 datastack_name = 'zlatic_octo_8x8x8_full_200525_datastack'
-client = CAVEclient(datastack_name=datastack_name, server_address=server_addrs)
+client = CAVEclient(datastack_name=datastack_name, server_address=server_addrs, auth_token=new_token)
+# #
+print(f"versions: {client.materialize.get_versions()}")
+#
+for version in client.materialize.get_versions():
+    print(f"Version {version}: {client.materialize.get_timestamp(version)}")
 
 # Try to find all leaves for a root-id
-root_id = 648518346363224861
+root_id = 648518346360884000 #648518346363224861
 # there are max of 7 levels. higher you go, the number of leaves decreases
 leaves = client.chunkedgraph.get_leaves(root_id, stop_layer=None)
 print(f"leaves: {leaves}, len of leaves {len(leaves)}")
 
+root_ids = [648518346360884739, 648518346370540737, 648518346364422034, 648518346360360026, 648518346362705549, 648518346364413735, 648518346363644529, 648518346362749310, 648518346362801892, 648518346367787881, 648518346362429739, 648518346363695060]
+
+
+print(f'change log: {client.chunkedgraph.get_change_log(root_id, filtered=False)}')
+print(f'merge log: {client.chunkedgraph.get_merge_log(root_id)}')
+print(f'tabular log: {client.chunkedgraph.get_tabular_change_log(root_id,filtered=False)}')
+print(f'original roots: {client.chunkedgraph.get_original_roots(root_id)}')
+print('timestamps')
+for id_ in root_ids:
+    print(f'{client.chunkedgraph.get_root_timestamps(id_)}')
+
+### Datetime analysis
+
+import datetime
+import pytz
+
+# 1. Your initial data, including empty lists.
+timestamps_with_empties = [
+    [datetime.datetime(2025, 6, 9, 10, 58, 0, 689000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 17, 14, 54, 14, 389000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 18, 12, 37, 9, 699000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 18, 15, 54, 26, 448000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 19, 11, 39, 0, 893000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 19, 14, 48, 30, 753000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 20, 8, 32, 7, 641000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 20, 10, 17, 1, 860000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 20, 14, 55, 39, 424000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 2, 9, 11, 21, 68000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 22, 20, 40, 55, 819000, tzinfo=pytz.UTC)],
+    [datetime.datetime(2025, 6, 23, 7, 57, 28, 453000, tzinfo=pytz.UTC)],
+]
+
+# 2. Filter out the empty lists and extract the datetime objects.
+# This list comprehension iterates through the outer list, and for each non-empty
+# inner list, it takes the first (and only) datetime object.
+valid_timestamps = [item[0] for item in timestamps_with_empties if item]
+
+# 3. Find the most recent (maximum) datetime object from the filtered list.
+latest_datetime = max(valid_timestamps)
+
+# 4. Convert the latest datetime object to a Unix timestamp and cast to an integer.
+unix_timestamp = int(latest_datetime.timestamp())
+
+# unix_timestamp = int(datetime.datetime(2025, 6, 21, 14, 55, 39, 424000, tzinfo=pytz.UTC).timestamp())
+
+print(f"Latest datetime object: {latest_datetime}")
+print(f"Converted Unix timestamp: {unix_timestamp}")
+
+# --- Verification ---
+# Latest datetime object: 2025-06-20 14:55:39.424000+00:00
+# Converted Unix timestamp: 1750431339
+
+
 input_leaves = [150994947, 1216348164]
 root_from_leaves = client.chunkedgraph.get_roots(input_leaves)
 print(f"root for leaves {root_from_leaves}")
+
+# bounds of a root_id
+root_info = client.chunkedgraph.get_roots([root_id], bounds=True)
+source_bbox = root_info[0]['bbox_mip0']
+print(source_bbox)
 
 # nodes = client.chunkedgraph.get_minimal_covering_nodes([root_id])
 # print(f"nodes from rootid {nodes}")
